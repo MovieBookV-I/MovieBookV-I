3084bea8be4732f3acffcea4ca88749684939db2
Ivaylo Arsov <arsov.ivaylo@gmail.com>
2016-12-27 17:47:20 +0200

    fixes

--- React-Movie-Book/src/Views/DeleteMovieView.js
+++ React-Movie-Book/src/Views/DeleteMovieView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import './CreateMovieView.css'
+import './DeleteMovieView.css'
 
 export default class DeleteMovieView extends Component {
     render() {
@@ -8,16 +8,27 @@ export default class DeleteMovieView extends Component {
             <div>
                 <div className="container">
                     <div className="jumbotron">
-                        <div className="row">
-                            <div className="col-md-3"></div>
-                            <div className="col-md-9">
-                                    <form className="create-movie-form form-horizontal" onSubmit={this.submitForm.bind(this)}>
-                                        <legend>Delete Movie:</legend>
-                                        <h4>{this.props.movieName}</h4>
-                                        <div>
-                                            <input type="submit" className="btn-danger" value="Confirm Delete"/>
-                                        </div>
-                                    </form>
+                        <div className="row deleteView">
+                            <div className="col-md-3">
+                                <form className="create-movie-form form-horizontal" onSubmit={this.submitForm.bind(this)}>
+                                    <legend>Delete Movie:</legend>
+                                    <h4>{this.props.movieName}</h4>
+                                    <div>
+                                        <input type="submit" className="btn-danger" value="Confirm Delete"/>
+                                    </div>
+                                </form>
+                            </div>
+                            <div id="deleteMoviePic" className="col-md-4">
+                                <img src={this.props.posterUrl} alt="poster"/>
+                            </div>
+                            <div className="col-md-4">
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <div>
+                                    <strong>Director:</strong> {this.props.directorName}
+                                </div>
                             </div>
                         </div>
                     </div>
